b65e9051c9bc1ada18d4a7d46829a9c5eee039b4
Mzt-t <mpbomil@gmail.com>
2022-06-16 22:36:33 +0200

    airbnb clone

--- airbnb/src/Card.js
+++ airbnb/src/Card.js
@@ -1,19 +1,20 @@
 import star from './images/star.png'
-export default function Card(props) {
+
+export default function Card({img,status,rating,amount,location,description,pricing}) {
     return(
     <div className="CardHolder">
-        <div className="Card" style={{backgroundImage: `url(${props.img})`}}>
-            <div className="Status">{props.status}</div>
+        <div className="Card" style={{backgroundImage: `url(${img})`}}>
+            <div className="Status">{status}</div>
         </div>
         <div className="CardInfo">
             <div className="RatingDiv">
                 <img src={star} alt="" className="star" />
-                <p className="Rating">&nbsp;{props.rating}&nbsp;</p>
-                <p className="RaterAmount">({props.amount}) ·&nbsp;</p>
-                <p className="Location">{props.location}</p>
+                <p className="Rating">&nbsp;{rating}&nbsp;</p>
+                <p className="RaterAmount">({amount}) ·&nbsp;</p>
+                <p className="Location">{location}</p>
             </div>
-            <p className="Description">{props.description}</p>
-            <p className="Pricing"><b>From ${props.pricing}</b> / person</p>
+            <p className="Description">{description}</p>
+            <p className="Pricing"><b>From ${pricing}</b> / person</p>
         </div>
     </div>
     )
